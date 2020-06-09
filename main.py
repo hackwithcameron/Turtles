afed9ef52333e3e997cc27e48b567bd8e702607f
Cameron <hackwithcameron@gmail.com>
2020-06-09 12:15:44 -0700

    Update main.py
    Fixed "autocreate turtle" bug when win function was called.

--- main.py
+++ main.py
@@ -11,36 +11,41 @@ class TurtleRace:
         self.screen_height = self.screen.window_height()
         self.players = []
         self.pen_size = 5
-        self.num_of_players = 7
+        self.num_of_players = 8
         self.turtle_spacing = self.screen_height / (self.num_of_players + 1)
         self.screen_top = self.screen_height / 2
         self.locations = []
         self.winner = ''
 
-        self.colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'grey']
+        self.colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'grey', 'chartreuse']
 
+    # main function to call helper functions.
     def run(self):
+        """
+        Main function that runs the TurtleRace class
+        :return:
+        """
+
         self.screen.clear()
         self.player()
         self.get_location()
         self.start()
         while not self.win():
             self.update()
+        turtle.ht()     # Hides auto created turtle
         turtle.write(self.winner, False, 'center', font=('Arial', 24, 'normal'))
         print('Press space bar to play again.')
         self.screen.listen()
         self.screen.onkey(TurtleRace().run, 'space')
         return
 
-    def start(self):
-        i = 0
-        for player in self.players:
-            player.pu()
-            player.goto(self.locations[i])
-            player.pd()
-            i += 1
-
+    # Creates each new turtle and appends to list of players
     def player(self):
+        """
+        Creates each new turtle
+        :return: list of players
+        """
+
         i = 0
         for player in range(self.num_of_players):
             player = turtle.Turtle()
@@ -51,6 +56,7 @@ class TurtleRace:
             i += 1
         return self.players
 
+    # Gets location for newly created turtle to move to.
     def get_location(self):
         for player in range(self.num_of_players):
             ylocation = self.screen_top - self.turtle_spacing * (player + 1)
@@ -58,10 +64,21 @@ class TurtleRace:
             self.locations.append(location)
         return self.locations
 
+    # Moves turtles to each location.
+    def start(self):
+        i = 0
+        for player in self.players:
+            player.pu()
+            player.goto(self.locations[i])
+            player.pd()
+            i += 1
+
+    # Moves turtles across screen
     def update(self):
         for player in self.players:
             player.fd(random.randrange(20))
 
+    # Check for a turtle to reach the finish and returns that turtles color.
     def win(self):
         for player in self.players:
             if player.xcor() > 290:
@@ -71,5 +88,4 @@ class TurtleRace:
 
 if __name__ == '__main__':
     TurtleRace().run()
-
     turtle.mainloop()
