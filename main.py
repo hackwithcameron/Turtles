d1e9793e2893149690801e47bb5823b288d0aab9
Cameron <hackwithcameron@gmail.com>
2020-06-09 13:04:32 -0700

    Docstrings
    Added comments and Docstrings

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-from collections import deque
 import turtle
 import random
 
 
 class TurtleRace:
+    """
+    Opens window and initiates a turtle race between multiple players.
+    """
 
     def __init__(self):
         self.screen = turtle.getscreen()
         self.screen_width = self.screen.window_width()
         self.screen_height = self.screen.window_height()
+        self.screen_top = self.screen_height / 2
+        self.screen.title('Turtle Race Game')
+
+        self.colors = ['red', 'green', 'blue', 'teal', 'yellow', 'orange', 'purple', 'grey', 'chartreuse', 'pink']
         self.players = []
         self.pen_size = 5
-        self.num_of_players = 8
+        # Number of players much not exceed number of colors
+        self.num_of_players = 10
+
         self.turtle_spacing = self.screen_height / (self.num_of_players + 1)
-        self.screen_top = self.screen_height / 2
         self.locations = []
         self.winner = ''
-
-        self.colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'grey', 'chartreuse']
 
     # main function to call helper functions.
     def run(self):
         """
         Main function that runs the TurtleRace class
-        :return:
         """
 
         self.screen.clear()
@@ -43,7 +47,7 @@ class TurtleRace:
     def player(self):
         """
         Creates each new turtle
-        :return: list of players
+        :return: List of players
         """
 
         i = 0
@@ -56,16 +60,24 @@ class TurtleRace:
             i += 1
         return self.players
 
-    # Gets location for newly created turtle to move to.
     def get_location(self):
+        """
+        Gets location for newly created turtle to move to.
+        Makes turtles space evenly down window regardless of
+        number of players.
+        :return: Location for specific turtle
+        """
         for player in range(self.num_of_players):
             ylocation = self.screen_top - self.turtle_spacing * (player + 1)
             location = [-300, int(ylocation)]
             self.locations.append(location)
         return self.locations
 
-    # Moves turtles to each location.
     def start(self):
+        """
+        Moves turtle to starting position.
+        :return:
+        """
         i = 0
         for player in self.players:
             player.pu()
@@ -75,11 +87,19 @@ class TurtleRace:
 
     # Moves turtles across screen
     def update(self):
+        """
+        Updates turtle position each frame.
+        :return: New location of each turtle
+        """
         for player in self.players:
             player.fd(random.randrange(20))
 
     # Check for a turtle to reach the finish and returns that turtles color.
     def win(self):
+        """
+        Checks location of each turtle for a winner.
+        :return: True, if a turtle has crossed the finish line.
+        """
         for player in self.players:
             if player.xcor() > 290:
                 self.winner = f'{player.color()[0].title()} wins!!!'
